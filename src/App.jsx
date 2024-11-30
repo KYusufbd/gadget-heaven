1e2812394f0252237e3670d7425ee43d2bb4f8c1
Kazi Yusuf <yfaka001@gmail.com>
2024-11-30 13:02:31 +0600

    Confgured react router successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router'
 import './App.css'
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <h1 className='text-3xl font-bold underline'>Gadget Heaven</h1>
+      <Outlet/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter, Route, Routes } from 'react-router';
 
 import './index.css'
 import App from './App.jsx'
@@ -8,7 +8,13 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route index element={<h1 className='text-5xl'>This is home page!</h1>}/>
+          <Route path='statistics' element={<h1 className='text-5xl'>This is statistics page!</h1>}/>
+          <Route path='dashboard' element={<h1 className='text-5xl'>This is dashboard!</h1>}/>
+        </Route>
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
