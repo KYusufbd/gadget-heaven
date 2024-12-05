c14d7a85fe8c1741670dc406560e518a344d94d8
Kazi Yusuf <yfaka001@gmail.com>
2024-12-05 10:27:29 +0600

    Fetched data and working on it.

--- src/GadgetCards.jsx
+++ src/GadgetCards.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 const GadgetCards = () => {
+  const [gadgets, setGadgets] =useState([])
+
+  useEffect(() => {
+    fetch('gadgets.json')
+    .then( res => res.json())
+    .then(data => setGadgets(data));
+  }, [])
+  console.log(gadgets); //Testing perpose only. Should be removed later.
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 mx-auto max-w-page-width bg-sky-100 py-8">
-        <div className="col-span-2 md:col-span-1 h-56 bg-sky-50">
-            <h3 className="text-xl">This is categories side bar</h3>
-        </div>
-        <div className="col-span-2 lg:col-span-3 h-56 bg-yellow-200">
-            <h3 className="text-xl">This is gadget cards portion</h3>
-        </div>
+    <div className="mx-auto grid max-w-page-width grid-cols-2 gap-4 bg-sky-100 py-8 md:grid-cols-3 md:gap-6 lg:grid-cols-4">
+      <div className="col-span-2 h-56 bg-sky-50 md:col-span-1">
+        <h3 className="text-xl">This is categories side bar</h3>
+      </div>
+      <div className="col-span-2 h-56 bg-yellow-200 lg:col-span-3">
+        <h3 className="text-xl">This is gadget cards portion</h3>
+      </div>
     </div>
   );
 };
