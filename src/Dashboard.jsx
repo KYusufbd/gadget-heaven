b9d72e472fe0db9d364b88671d1ff1851e8229c9
Kazi Yusuf <138551075+KYusufbd@users.noreply.github.com>
2025-01-06 17:37:53 +0600

    Added Dashboard component. Now time to complete it.

---
+++ src/Dashboard.jsx
@@ -0,0 +1,15 @@
+
+const Dashboard = () => {
+    return (
+        <div className="w-full bg-bgcol">
+            <div className="flex flex-col gap-8 text-center bg-primary px-5 py-8 text-white">
+                <div className="flex flex-col gap-4">
+                    <h1 className="text-3xl font-bold">Dashboard</h1>
+                    <p className="mx-auto max-w-3xl text-base">Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Dashboard;
