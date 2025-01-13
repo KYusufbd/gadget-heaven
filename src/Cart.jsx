6255df04b1c7c2e31e788f0ed5223fb2bc7ee114
Kazi Yusuf <138551075+KYusufbd@users.noreply.github.com>
2025-01-13 21:40:07 +0600

    Working on cart section

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -8,7 +8,23 @@ const Cart = () => {
     const {cart, setCart} = useContext(DashboardContext);
 
     return (
-        <h1 className="text-center text-2xl font-bold">This is Cart section!</h1>
+        <div className="flex flex-col gap-8 w-full bg-bgcol px-4">
+            <div className="w-page-width max-w-full mx-auto flex flex-row justify-between">
+                <h5 className="text-2xl font-bold">Cart</h5>
+                <div>
+                    <h5 className="text-2xl font-bold">Total cost: 999.99</h5>
+                </div>
+            </div>
+            <div className="mx-auto max-w-page-width bg-white">
+                {gadgets.map(gadget => {
+                    if (cart.includes(gadget.product_id)) {
+                        return (
+                            <h1>{gadget?.product_title}</h1>
+                        )
+                    }
+                })}
+            </div>
+        </div>
     )
 };
 
